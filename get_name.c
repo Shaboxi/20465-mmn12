5c8ac390edba6428d69113eaa4531e320f647377
asaf129@gmail.com <asaf129@gmail.com>
2019-12-16 23:37:20 +0200

    start get_name

--- get_name.c
+++ get_name.c
@@ -11,51 +11,73 @@ void copy_string(char *target, char *source)
     *target = '\0';
 }
 
-void string_to_lower (char* str)
+
+
+int compare (char* str, char* compare)
 {
-    int i;
+    int result;
 
-
-    for(i = 0; str[i]; i++){
-        str[i] = tolower(str[i]);
-    }
+    while (*str) 
+    {
+        result = tolower(*str) - tolower(*compare);
+        if (result) {
+            return result;
+        } 
+        str++;
+        compare++;
+    } 
+    return 0;
     
     
 }
 
 char arr[NUMBER_OF_STRINGS][MAX_STRING_SIZE];
 
-int validate_input (int validateIndex ) /*, char arrayOfString[NUMBER_OF_STRINGS][MAX_STRING_SIZE])*/
+
+int validate_input (int validateIndex )
 {
     int i = 0;
-    
+    int counter = 0; /* how many times a name is in the array */
     char dummy[MAX_STRING_SIZE];
     
 
     /* copy strings, so the real value in the array will be kept */
     copy_string(dummy, arr[validateIndex]);
     
-    /* get the name in a lowercase */ 
+    /* get the name in a lowercase  
     string_to_lower(dummy);
     
-    
+    */
 
     while(i <= validateIndex)
     {
-        printf("\n Compare: String to lower: %s", dummy);
-        printf(", i= %s \n", arr[i]);
+
+        printf("The array: on index %d you can find the value %s\n", i, arr[i]);
 
         /* 
         if the name is 2 times in the array - the input is not valid. 
         1 time for its original, 
         1 time for duplication.
         */
+        if(compare(dummy,arr[i]) == 0)
+            counter++;
+
+        if(counter == 2)
+        {
+            printf("\n Compare: String to lower: %s", dummy);
+            printf(", = %s \n", arr[i]);
+            return 1;
+        }
+
+
+
+
         i++;
     }
 
-        
-      /*printf("\n %s", stringToCompare);*/
-      return 1;
+    
+    /*printf("\n %s", stringToCompare);*/
+    return 0;
 }
 
 void get_name() 
@@ -74,35 +96,18 @@ int main()
     /* handle input */
     for(inputIndex = 0; inputIndex <= NUMBER_OF_STRINGS; inputIndex++)
     {
-        printf("\nEnter a NAME: ");
+        printf("\nEnter a NAME: \n");
         scanf("%s", arr[inputIndex]);
 
-        /* 
-        cast the array so it could be sent to the function
-        printf("\nInput value: %s", arr[inputIndex]);
-        */
-        
-        
-        validate_input(inputIndex);
-        
+        if(validate_input(inputIndex) != 0)
+        {
+            printf("The name %s was already enterd! Exit program. \n" , arr[inputIndex]);
+            return 0;
+        }
+             
     }
-        printf("\nString Value: %s", arr[inputIndex-1]);
-        
-    
         
         
-        
-        /*printf("\nString index %d: ", "1");
-        
-        
-        */
-       
-
-        
-        
-        printf("\nString Value: %s", arr[inputIndex-1]);
-
-  
 
     /* call get_name funcation */
     get_name(); 
